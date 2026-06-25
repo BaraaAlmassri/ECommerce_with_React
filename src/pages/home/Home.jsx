fd9cc3d0f9fbca509b9245c48647ae231927c9a7
BaraaAlmassri <chrollo12388@gmail.com>
2026-06-26 01:04:41 +0300

    create register form using mui

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+   <Typography component='h1' variant='h1' sx={{fontSize:20}}>Hello , world !</Typography>
+</div>
   )
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,34 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 export default function Register() {
+   const RegisterForm = async (data)=>{
+     try{
+          const response = await axios.post(`${import.meta.env.VITE_BURL}/auth/Account/Register` , data);
+          console.log(response);
+     }catch(err) {
+            console.log(err);
+     }
+   }
+   const {register , handleSubmit} = useForm();
+
   return (
-    <div>Register</div>
+    <Box onSubmit={handleSubmit(RegisterForm)} component='section' className='registerPage'>
+      <Typography component='h1' variant='h2'> Register</Typography>
+
+      <Box component='form' sx={{marginTop:2 , display:'flex' , flexDirection:'column' , gap:2}}>
+              <TextField fullWidth {...register("userName")} label="UserName" variant="outlined" />
+              <TextField fullWidth {...register("fullName")} label="FullName" variant="outlined" />
+              <TextField fullWidth {...register("email")} label="Email" variant="outlined" />
+              <TextField fullWidth {...register("phoneNumber")} label="PhoneNumber" variant="outlined" />
+              <TextField fullWidth {...register("password")} label="Password" variant="outlined" />
+
+              <Button variant='contained' type='submit' sx={{width :100 , marginLeft:'auto' , marginRight:'auto'}}>Register</Button>
+
+      </Box>
+    </Box>
   )
 }
