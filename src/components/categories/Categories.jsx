64a561ec76cc014d861add28dc0010aa22ca72f7
BaraaAlmassri <chrollo12388@gmail.com>
2026-06-27 18:22:56 +0300

    get categories

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,20 @@
+import { Category } from '@mui/icons-material';
+import { Box, CircularProgress, Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import axios from "axios";
+import React from 'react'
+import useCategories from '../../hooks/useCategories';
+
+export default function Categories() {
+        
+    const {data,isLoading , isError , error } = useCategories();
+
+    if(isLoading) return <CircularProgress></CircularProgress>
+    if(isError) return <Typography color='red'>{error}</Typography>
+
+  return (
+    <div>
+        {data.response.data.map((category)=> <Box> <Typography>{category.name} </Typography> </Box>)}
+        </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
+import Categories from '../../components/categories/Categories';
 
 export default function Home() {
   return (
-    <div>
-   <Typography component='h1' variant='h1' sx={{fontSize:20}}>Hello , world !</Typography>
-</div>
+    <>
+  <Categories></Categories>
+</>
   )
 }
